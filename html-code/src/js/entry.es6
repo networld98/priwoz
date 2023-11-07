cfea0eeb503a2beb25b1fe6d27e47a7a65c6bff6
lesniakolha <olha.lesniak.triare@gmail.com>
2023-11-07 19:25:07 +0100

    project setup

---
+++ html-code/src/js/entry.es6
@@ -0,0 +1,12 @@
+require("../sass/style.scss");
+
+require("../fonts/fontello/css/fontello.css");
+
+
+
+(function ($) {
+
+    $(".hero").length > 0 && require("./template-parts/hero.es6");
+
+
+})(jQuery);
---
+++ html-code/webpack.config.js
@@ -0,0 +1,113 @@
+const NODE_ENV = process.env.NODE_ENV || "development";
+const webpack = require("webpack");
+const autoprefixer = require("autoprefixer");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+module.exports = {
+
+    watch: true,
+
+    entry: ["babel-polyfill", "./src/js/entry.es6"],
+
+    output: {
+        path: __dirname + "/build",
+        filename: "bundle.js"
+    },
+
+
+    plugins: [
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify(NODE_ENV)
+        }),
+        new webpack.ProvidePlugin({
+            Promise: "es6-promise-promise"
+        }),
+        new MiniCssExtractPlugin(),
+        new ESLintPlugin({
+            extensions: "es6",
+        }),
+    ],
+
+    module: {
+
+        rules: [
+            {
+                test: /\.es6$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: NODE_ENV == "development",
+                        }
+                    },
+                    {
+                        loader: "resolve-url-loader",
+                        options: {
+                            sourceMap: NODE_ENV == "development"
+                        }
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    autoprefixer
+                                ],
+                            },
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: NODE_ENV == "development"
+                        }
+                    }
+                ],
+            },
+            {
+                test: /\.(png|svg|gif)$/,
+                type: 'asset/resource',
+
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: './fonts/[name][ext]',
+                },
+            }
+
+        ]
+    },
+
+    devtool: NODE_ENV == "development" ? "source-map" : false,
+
+
+    externals: {
+        "jquery": "jQuery"
+    }
+
+};
+
+
+if (NODE_ENV == "production") {
+    module.exports.optimization = {
+        minimize: true,
+        runtimeChunk: false,
+    }
+}
