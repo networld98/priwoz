277cc034583c4b1548a495d6c0733510e41982ef
lesniakolha <olha.lesniak.triare@gmail.com>
2023-12-23 00:39:08 +0100

    products overview subcategory page

---
+++ html-code/products-overview-subcategory.php
@@ -0,0 +1,7 @@
+<?php include './template-parts/partials/header.php'; ?>
+
+<?php include './template-parts/partials/breadcrumbs-products-overview-subcategory.php'; ?>
+
+<?php include './template-parts/products-overview-subcategory-section.php'; ?>
+
+<?php include './template-parts/partials/footer.php'; ?>
---
+++ html-code/template-parts/partials/breadcrumbs-products-overview-subcategory.php
@@ -0,0 +1,7 @@
+<section class="breadcrumbs-section">
+    <div class="container">
+        <div class="breadcrumbs">
+            <a href="/">Priwoz</a> <span class="sep">-</span> <a href="/">Доска объявлений</a> <span class="sep">-</span> <a href="/">Авто</a> <span class="sep">-</span> <span class="current-page">Запасные части</span>
+        </div>
+    </div>
+</section>
