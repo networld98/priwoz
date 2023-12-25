d1f52a05afad6d92d3cbd00d143208733c25a48d
lesniakolha <olha.lesniak.triare@gmail.com>
2023-12-25 21:45:11 +0100

    products overview company page

---
+++ html-code/products-overview-company.php
@@ -0,0 +1,7 @@
+<?php include './template-parts/partials/header.php'; ?>
+
+<?php include './template-parts/partials/breadcrumbs-products-overview-company.php'; ?>
+
+<?php include './template-parts/products-overview-company-section.php'; ?>
+
+<?php include './template-parts/partials/footer.php'; ?>
---
+++ html-code/template-parts/partials/breadcrumbs-products-overview-company.php
@@ -0,0 +1,7 @@
+<section class="breadcrumbs-section">
+    <div class="container">
+        <div class="breadcrumbs">
+            <a href="/">Priwoz</a> <span class="sep">-</span> <a href="/">Доска объявлений</a> <span class="sep">-</span> <a href="/">Компании</a> <span class="sep">-</span> <span class="current-page">VICTORY transport travel company</span>
+        </div>
+    </div>
+</section>
