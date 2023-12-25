e4c8fce71f7e519f44aaa09ebe68bca441df305a
lesniakolha <olha.lesniak.triare@gmail.com>
2023-12-25 18:32:35 +0100

    products overview author page

---
+++ html-code/products-overview-author.php
@@ -0,0 +1,7 @@
+<?php include './template-parts/partials/header.php'; ?>
+
+<?php include './template-parts/partials/breadcrumbs-products-overview-author.php'; ?>
+
+<?php include './template-parts/products-overview-author-section.php'; ?>
+
+<?php include './template-parts/partials/footer.php'; ?>
---
+++ html-code/template-parts/partials/breadcrumbs-products-overview-author.php
@@ -0,0 +1,7 @@
+<section class="breadcrumbs-section">
+    <div class="container">
+        <div class="breadcrumbs">
+            <a href="/">Priwoz</a> <span class="sep">-</span> <a href="/">Доска объявлений</a> <span class="sep">-</span> <a href="/">Авторы объявлений</a> <span class="sep">-</span> <span class="current-page">Галанчановски</span>
+        </div>
+    </div>
+</section>
