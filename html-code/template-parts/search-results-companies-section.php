5b6721953d0dcc6d84452e6eda90ddf763405854
lesniakolha <olha.lesniak.triare@gmail.com>
2024-01-02 23:35:49 +0100

    search results (companies)

---
+++ html-code/template-parts/search-results-companies-section.php
@@ -0,0 +1,119 @@
+<section class="products-overview-section">
+    <div class="container">
+
+        <div class="search-title-box">
+            <h1>Результаты поиска <span class="highlight">Клининг:</span></h1>
+            <div class="search-output"><span class="current">компаний 3</span> <a href="/">объявлений 22</a></div>
+        </div>
+
+        <div class="filter-output-box">
+            <div class="filter-output">
+                <div class="links"><a href="/">Priwoz</a> <span class="sep">-</span> <a href="/">Диски Original Renault
+                        Master</a> <span class="sep">/</span> <span class="current-page">Варна</span></div>
+            </div>
+
+            <div class="sort-by-box">
+                <div class="bx_filter_parameters_box active">
+                    <div class="bx_filter_parameters_box_title">Сортировать по</div>
+                    <div class="bx_filter_block">
+                        <div class="bx_filter_parameters_box_container">
+                            <select class="form-select -without-search">
+                                <option value="new" selected>сначала новые</option>
+                                <option value="low">сначала дешевле</option>
+                                <option value="high">сначала дороже</option>
+                            </select>
+                        </div>
+                        <div class="clb"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="companies-wrap">
+            <div class="grid companies-masonry">
+                <div class="grid-sizer"></div>
+                <div class="gutter-sizer"></div>
+                <div class="grid-item company-grid-item">
+                    <a href="/" class="box">
+                        <div class="img">
+                            <img class="bg-img" src="/html-code/temporary-images/home/company1.png"
+                                 alt="Priwoz company">
+                            <img class="company-logo" src="/html-code/temporary-images/home/company-logo1.png"
+                                 alt="Priwoz company logo">
+                        </div>
+                        <div class="text">
+                            <h2 class="company-title">Натали</h2>
+                            <div class="category-location">
+                                <div class="category">Красота</div>
+                                <div class="location">Бургас</div>
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#" class="add-to-favourite">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+                            <path d="M7.22704 3.7542C7.89956 2.0143 8.23581 1.14435 8.78212 1.02378C8.92577 0.992074 9.07423 0.992074 9.21788 1.02378C9.76419 1.14435 10.1004 2.0143 10.773 3.7542C11.1554 4.74364 11.3466 5.23837 11.7044 5.57486C11.8048 5.66924 11.9137 5.7533 12.0297 5.82585C12.4433 6.08452 12.9596 6.13251 13.9921 6.22847C15.74 6.39092 16.6139 6.47214 16.8808 6.98926C16.936 7.09636 16.9736 7.21232 16.9919 7.33231C17.0804 7.9117 16.438 8.51829 15.153 9.73148L14.7962 10.0684C14.1954 10.6356 13.8951 10.9192 13.7213 11.2731C13.6171 11.4854 13.5472 11.714 13.5145 11.9499C13.4599 12.343 13.5479 12.7544 13.7238 13.5772L13.7866 13.8712C14.1021 15.3468 14.2599 16.0846 14.0629 16.4473C13.8861 16.773 13.5603 16.9816 13.2004 16.9994C12.7997 17.0193 12.2352 16.5419 11.1061 15.5871C10.3622 14.958 9.99029 14.6435 9.5774 14.5206C9.20007 14.4084 8.79993 14.4084 8.4226 14.5206C8.00971 14.6435 7.63777 14.958 6.89389 15.5871C5.7648 16.5419 5.20026 17.0193 4.79961 16.9994C4.43972 16.9816 4.11393 16.773 3.93705 16.4473C3.74015 16.0846 3.89789 15.3468 4.21336 13.8712L4.27621 13.5772C4.45213 12.7544 4.54009 12.343 4.4855 11.9499C4.45276 11.714 4.38288 11.4854 4.27866 11.2731C4.10493 10.9192 3.80456 10.6356 3.20382 10.0684L2.847 9.73149C1.56205 8.5183 0.919574 7.9117 1.00805 7.33231C1.02638 7.21232 1.06396 7.09636 1.11923 6.98926C1.38611 6.47214 2.26005 6.39092 4.00792 6.22847C5.04044 6.13251 5.55671 6.08452 5.97026 5.82585C6.08626 5.7533 6.19521 5.66924 6.29557 5.57486C6.65337 5.23837 6.8446 4.74364 7.22704 3.7542Z"
+                                  stroke="currentColor"/>
+                        </svg>
+                    </a>
+                </div>
+                <div class="grid-item company-grid-item">
+                    <a href="/" class="box">
+                        <div class="img">
+                            <img class="bg-img" src="/html-code/temporary-images/home/company3.png"
+                                 alt="Priwoz company">
+                            <img class="company-logo" src="/html-code/temporary-images/home/company-logo3.png"
+                                 alt="Priwoz company logo">
+                        </div>
+                        <div class="text">
+                            <h2 class="company-title">Memorial</h2>
+                            <div class="category-location">
+                                <div class="category">медицина</div>
+                                <div class="location">София + 3</div>
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#" class="add-to-favourite">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+                            <path d="M7.22704 3.7542C7.89956 2.0143 8.23581 1.14435 8.78212 1.02378C8.92577 0.992074 9.07423 0.992074 9.21788 1.02378C9.76419 1.14435 10.1004 2.0143 10.773 3.7542C11.1554 4.74364 11.3466 5.23837 11.7044 5.57486C11.8048 5.66924 11.9137 5.7533 12.0297 5.82585C12.4433 6.08452 12.9596 6.13251 13.9921 6.22847C15.74 6.39092 16.6139 6.47214 16.8808 6.98926C16.936 7.09636 16.9736 7.21232 16.9919 7.33231C17.0804 7.9117 16.438 8.51829 15.153 9.73148L14.7962 10.0684C14.1954 10.6356 13.8951 10.9192 13.7213 11.2731C13.6171 11.4854 13.5472 11.714 13.5145 11.9499C13.4599 12.343 13.5479 12.7544 13.7238 13.5772L13.7866 13.8712C14.1021 15.3468 14.2599 16.0846 14.0629 16.4473C13.8861 16.773 13.5603 16.9816 13.2004 16.9994C12.7997 17.0193 12.2352 16.5419 11.1061 15.5871C10.3622 14.958 9.99029 14.6435 9.5774 14.5206C9.20007 14.4084 8.79993 14.4084 8.4226 14.5206C8.00971 14.6435 7.63777 14.958 6.89389 15.5871C5.7648 16.5419 5.20026 17.0193 4.79961 16.9994C4.43972 16.9816 4.11393 16.773 3.93705 16.4473C3.74015 16.0846 3.89789 15.3468 4.21336 13.8712L4.27621 13.5772C4.45213 12.7544 4.54009 12.343 4.4855 11.9499C4.45276 11.714 4.38288 11.4854 4.27866 11.2731C4.10493 10.9192 3.80456 10.6356 3.20382 10.0684L2.847 9.73149C1.56205 8.5183 0.919574 7.9117 1.00805 7.33231C1.02638 7.21232 1.06396 7.09636 1.11923 6.98926C1.38611 6.47214 2.26005 6.39092 4.00792 6.22847C5.04044 6.13251 5.55671 6.08452 5.97026 5.82585C6.08626 5.7533 6.19521 5.66924 6.29557 5.57486C6.65337 5.23837 6.8446 4.74364 7.22704 3.7542Z"
+                                  stroke="currentColor"/>
+                        </svg>
+                    </a>
+                </div>
+                <div class="grid-item company-grid-item">
+                    <div class="box -video video-open-trigger"
+                         data-video="/html-code/temporary-images/home/video_example.mp4">
+                        <div class="img">
+                            <img class="bg-img" src="/html-code/temporary-images/products/company-video.png"
+                                 alt="Priwoz company">
+                            <img class="company-logo" src="/html-code/temporary-images/home/company-logo4.png"
+                                 alt="Priwoz company logo">
+                            <div class="date">16 октября 2023</div>
+                        </div>
+                        <div class="text">
+                            <h2 class="company-title">Как мне почистили диван в ChistoClean</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="grid-item company-grid-item">
+                    <a href="/" class="box">
+                        <div class="text">
+                            <h2 class="company-title">Сhartershop</h2>
+                            <div class="category-location">
+                                <div class="category">Грузовые перевозки</div>
+                                <div class="location">Варна + 10</div>
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#" class="add-to-favourite">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+                            <path d="M7.22704 3.7542C7.89956 2.0143 8.23581 1.14435 8.78212 1.02378C8.92577 0.992074 9.07423 0.992074 9.21788 1.02378C9.76419 1.14435 10.1004 2.0143 10.773 3.7542C11.1554 4.74364 11.3466 5.23837 11.7044 5.57486C11.8048 5.66924 11.9137 5.7533 12.0297 5.82585C12.4433 6.08452 12.9596 6.13251 13.9921 6.22847C15.74 6.39092 16.6139 6.47214 16.8808 6.98926C16.936 7.09636 16.9736 7.21232 16.9919 7.33231C17.0804 7.9117 16.438 8.51829 15.153 9.73148L14.7962 10.0684C14.1954 10.6356 13.8951 10.9192 13.7213 11.2731C13.6171 11.4854 13.5472 11.714 13.5145 11.9499C13.4599 12.343 13.5479 12.7544 13.7238 13.5772L13.7866 13.8712C14.1021 15.3468 14.2599 16.0846 14.0629 16.4473C13.8861 16.773 13.5603 16.9816 13.2004 16.9994C12.7997 17.0193 12.2352 16.5419 11.1061 15.5871C10.3622 14.958 9.99029 14.6435 9.5774 14.5206C9.20007 14.4084 8.79993 14.4084 8.4226 14.5206C8.00971 14.6435 7.63777 14.958 6.89389 15.5871C5.7648 16.5419 5.20026 17.0193 4.79961 16.9994C4.43972 16.9816 4.11393 16.773 3.93705 16.4473C3.74015 16.0846 3.89789 15.3468 4.21336 13.8712L4.27621 13.5772C4.45213 12.7544 4.54009 12.343 4.4855 11.9499C4.45276 11.714 4.38288 11.4854 4.27866 11.2731C4.10493 10.9192 3.80456 10.6356 3.20382 10.0684L2.847 9.73149C1.56205 8.5183 0.919574 7.9117 1.00805 7.33231C1.02638 7.21232 1.06396 7.09636 1.11923 6.98926C1.38611 6.47214 2.26005 6.39092 4.00792 6.22847C5.04044 6.13251 5.55671 6.08452 5.97026 5.82585C6.08626 5.7533 6.19521 5.66924 6.29557 5.57486C6.65337 5.23837 6.8446 4.74364 7.22704 3.7542Z"
+                                  stroke="currentColor"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
