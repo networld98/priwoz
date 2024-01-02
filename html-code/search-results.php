ac6a36030088ff402f85c34842dffe9d035130c8
lesniakolha <olha.lesniak.triare@gmail.com>
2024-01-02 23:17:02 +0100

    search results (products)

---
+++ html-code/search-results.php
@@ -0,0 +1,5 @@
+<?php include './template-parts/partials/header.php'; ?>
+
+<?php include './template-parts/search-results-section.php'; ?>
+
+<?php include './template-parts/partials/footer.php'; ?>
