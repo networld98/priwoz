a965c62786b530ce28e3d55435438146ba21119c
lesniakolha <olha.lesniak.triare@gmail.com>
2024-01-12 00:45:31 +0100

    404 page

---
+++ html-code/404.php
@@ -0,0 +1,5 @@
+<?php include './template-parts/partials/header-sign-in.php'; ?>
+
+<?php include './template-parts/404-section.php'; ?>
+
+<?php include './template-parts/partials/footer.php'; ?>
---
+++ html-code/template-parts/404-section.php
@@ -0,0 +1,21 @@
+<section class="not-found-section">
+    <div class="container">
+        <div class="bg-box">
+            <div class="row">
+                <div class="col-xs-12 col-md-6 col-xl-4">
+                    <h1 class="title">404</h1>
+                    <div class="back-text">
+                        Что-то пошло не так. Можно воспользоваться меню сайта или вернуться на
+                        <a href="/">главную</a>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-6 col-xl-5">
+                    <div class="img">
+                        <img src="/html-code/images/404-cat.png" alt="404 Priwoz">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
