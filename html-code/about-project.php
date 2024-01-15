23c92be63f97e56f12ebeba547146ec8e6ecd14a
lesniakolha <olha.lesniak.triare@gmail.com>
2024-01-16 00:09:18 +0100

    about page

---
+++ html-code/about-project.php
@@ -0,0 +1,9 @@
+<?php include './template-parts/partials/header.php'; ?>
+
+<?php include './template-parts/partials/breadcrumbs-about.php'; ?>
+
+<?php include './template-parts/about-project-section.php'; ?>
+
+<?php include './template-parts/partnership-section.php'; ?>
+
+<?php include './template-parts/partials/footer.php'; ?>
