d9a81545f8c2c034553a327f1b6c149d54f9c396
netwo <stercenko.sergej@gmail.com>
2024-01-19 10:33:24 +0200

    добавил урлреврайт для укр версии

---
+++ urlrewrite.php
@@ -0,0 +1,84 @@
+<?php
+$arUrlRewrite = array(
+    21 =>
+        array(
+            'CONDITION' => '#^/personal/ads-list/#',
+            'RULE' => '',
+            'ID' => 'bitrix:news',
+            'PATH' => '/personal/ads-list/index.php',
+            'SORT' => 100,
+        ),
+    24 =>
+        array(
+            'CONDITION' => '#^/search/#',
+            'RULE' => 'SMART_FILTER_PATH=$1&$2',
+            'ID' => 'networld:catalog.smart.filter',
+            'PATH' => '/search/index.php',
+            'SORT' => 100,
+        ),
+    19 =>
+        array(
+            'CONDITION' => '#^/companies/#',
+            'RULE' => '',
+            'ID' => 'bitrix:news',
+            'PATH' => '/companies/index.php',
+            'SORT' => 100,
+        ),
+    20 =>
+        array(
+            'CONDITION' => '#^/blog/#',
+            'RULE' => '',
+            'ID' => 'bitrix:news',
+            'PATH' => '/blog/index.php',
+            'SORT' => 100,
+        ),
+    23 =>
+        array(
+            'CONDITION' => '#^/ads/#',
+            'RULE' => '',
+            'ID' => 'bitrix:news',
+            'PATH' => '/ads/index.php',
+            'SORT' => 100,
+        ),
+
+    25 =>
+        array(
+            'CONDITION' => '#^/ua/personal/ads-list/#',
+            'RULE' => '',
+            'ID' => 'bitrix:news',
+            'PATH' => '/ua/personal/ads-list/index.php',
+            'SORT' => 100,
+        ),
+    26 =>
+        array(
+            'CONDITION' => '#^/ua/search/#',
+            'RULE' => 'SMART_FILTER_PATH=$1&$2',
+            'ID' => 'networld:catalog.smart.filter',
+            'PATH' => '/ua/search/index.php',
+            'SORT' => 100,
+        ),
+    27 =>
+        array(
+            'CONDITION' => '#^/ua/companies/#',
+            'RULE' => '',
+            'ID' => 'bitrix:news',
+            'PATH' => '/ua/companies/index.php',
+            'SORT' => 100,
+        ),
+    28 =>
+        array(
+            'CONDITION' => '#^/ua/blog/#',
+            'RULE' => '',
+            'ID' => 'bitrix:news',
+            'PATH' => '/ua/blog/index.php',
+            'SORT' => 100,
+        ),
+    29 =>
+        array(
+            'CONDITION' => '#^/ua/ads/#',
+            'RULE' => '',
+            'ID' => 'bitrix:news',
+            'PATH' => '/ua/ads/index.php',
+            'SORT' => 100,
+        ),
+);
