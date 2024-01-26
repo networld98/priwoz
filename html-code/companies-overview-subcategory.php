887e654bdddd41f60e044e553435e1b735e66484
lesniakolha <olha.lesniak.triare@gmail.com>
2024-01-27 00:57:28 +0100

    companies overview subcategory + html

---
+++ html-code/companies-overview-subcategory.php
@@ -0,0 +1,11 @@
+<?php include './template-parts/partials/header.php'; ?>
+
+<?php include './template-parts/partials/breadcrumbs-companies-overview-subcategory.php'; ?>
+
+<?php include './template-parts/companies-overview-subcategory-section.php'; ?>
+
+<?php include './template-parts/products-overview-min-section.php'; ?>
+
+<?php include './template-parts/partnership-section.php'; ?>
+
+<?php include './template-parts/partials/footer.php'; ?>
---
+++ html-code/template-parts/partials/breadcrumbs-companies-overview-subcategory.php
@@ -0,0 +1,7 @@
+<section class="breadcrumbs-section">
+    <div class="container">
+        <div class="breadcrumbs">
+            <a href="/">Priwoz</a> <span class="sep">-</span> <a href="/">Компании</a> <span class="sep">-</span>  <a href="/">Транспорт</a> <span class="sep">-</span> <span class="current-page">Пассажирские перевозки</span>
+        </div>
+    </div>
+</section>
