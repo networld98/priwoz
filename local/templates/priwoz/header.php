08c33a6de2e93601559580b55d7ccbc66235c42e
netwo <stercenko.sergej@gmail.com>
2024-02-17 22:45:13 +0200

    убрал странный баг которого небыло с http авторизацией

--- local/templates/priwoz/header.php
+++ local/templates/priwoz/header.php
@@ -1,6 +1,7 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 IncludeTemplateLangFile(__FILE__);
 CJSCore::Init(array('jquery3'));
+CModule::IncludeModule('iblock');
 $curPagePath = $APPLICATION->GetCurDir();
 $curPagePath = explode("/", $curPagePath);
 if (SITE_ID == 's1') {
