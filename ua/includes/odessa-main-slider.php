ba10a6e0e13c80bb298289c2431b42707fcb764e
netwo <stercenko.sergej@gmail.com>
2024-03-07 13:36:05 +0200

    Поправил ошибку с номером

--- ua/includes/odessa-main-slider.php
+++ ua/includes/odessa-main-slider.php
@@ -9,8 +9,8 @@ while (($data = fgetcsv($src, 0, "\t")) !== FALSE) {
 } ?>
 <div class="swiper-wrapper">
     <? for ($i = 0; $i < 10; $i++) {
-        $random_text_number = rand(0, count((array)$textArray));
-        $random_image_number = rand(1, count((array)$textArray));
+        $random_text_number = rand(0, count((array)$textArray)-1);
+        $random_image_number = rand(1, count((array)$textArray)-1);
         $url = 'https://priwoz.info'.SITE_DIR.'includes/odessa-slider/'.$random_image_number.'.jpg';
         $file_content = @file_get_contents($url);
         if ($file_content === FALSE) {
