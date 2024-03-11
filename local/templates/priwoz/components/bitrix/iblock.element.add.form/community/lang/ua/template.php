9db8b0a3a53e65027c747922e9e64fe2f0e9f9b3
netwo <stercenko.sergej@gmail.com>
2024-03-11 12:15:30 +0200

    Добавил шаблон и перевод в гит

---
+++ local/templates/priwoz/components/bitrix/iblock.element.add.form/community/lang/ua/template.php
@@ -0,0 +1,20 @@
+<?
+$MESS["IBLOCK_FIRST_PHOTO"] = 'Перше фото буде на обкладинці оголошення. Перетягніть, щоб змінити порядок.';
+$MESS["IBLOCK_FILE_SIZE"] = 'Допустимі формати: png, jpg, pdf до 5 mb';
+$MESS["IBLOCK_CABINET_560"] = 'Логотип *';
+$MESS["IBLOCK_CABINET_579"] = 'Фон *';
+$MESS["IBLOCK_CABINET_PREVIEW_TEXT"] = 'Опис *';
+$MESS["IBLOCK_CABINET_561"] = 'Адреса спільноти *';
+$MESS["IBLOCK_CABINET_578"] = 'Контакт адміністратора *';
+$MESS["IBLOCK_CABINET_NAME"] = 'Назва *';
+$MESS ['FORM_ADD_COMM'] = "Додати спільноту";
+$MESS ['FORM_WRITE'] = "Заповніть поля нижче";
+$MESS ['FORM_REQUIRED_FIELDS'] = "* Обов'язково для заповнення";
+$MESS ['FORM_APPLY'] = "Застосувати";
+$MESS ['FORM_ADD'] = "Додати";
+$MESS ['FORM_ACCESS_DENIED'] = "Не вистачає прав доступу до веб-форми.";
+$MESS ['FORM_DATA_SAVED1'] = "Дякую!<br><br>Ваша заявка №";
+$MESS ['FORM_DATA_SAVED2'] = "прийнята до розгляду.";
+$MESS ['FORM_MODULE_NOT_INSTALLED'] = "Модуль веб-форм не встановлений.";
+$MESS ['FORM_NOT_FOUND'] = "Веб-форма не знайдена.";
+$MESS ['FORM_NOTE_ADDOK'] = '<div class="success-box"><p>Спільнота додана і буде доступна після модерації, спасибі</p><p>Щоб ви так жили як ми вас чекаємо!</p></div> ';
